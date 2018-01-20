e96071c9aa00175847f6aebbc1601a64f2fd226c
Amit Kumar <amkt18@live.com>
2018-01-20 16:52:09 +0530

    Day2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyComponentComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
