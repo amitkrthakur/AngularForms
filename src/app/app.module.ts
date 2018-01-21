de291815ee3c64f4d4d2cdfc670a22435464f846
Amit Kumar <amkt18@live.com>
2018-01-21 05:31:19 +0530

    router links established

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
@@ -15,14 +16,19 @@ import { FormdetailComponent } from './formdetail/formdetail.component';
     AppComponent,
     MyComponentComponent,
     FormComponent,
-    FormdetailComponent
+    FormdetailComponent,
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot([
+      { path: '', component: FormComponent },
+      { path: 'about', component: FormdetailComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
