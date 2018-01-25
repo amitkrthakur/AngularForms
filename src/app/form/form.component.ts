7b0d7947211e640eb76698401531150bc20bde4a
Amit Kumar <amkt18@live.com>
2018-01-25 15:15:25 +0530

    tried mat-list and mat-options

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,23 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+// import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+
+  // animations: [
+
+  //   trigger('goals', [
+  //     transition('* => *', [
+
+  //       query(':enter', style({ opacity: 0 }), {optional: true}),
+
+  //       query(':enter', stagger('300ms', [
+  //         animate('.6s ease-in', keyframes([
+  //           style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+  //           style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+  //           style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+  //         ]))]), {optional: true})
+  //     ])
+  //   ])
+
+  // ]
+
 })
 export class FormComponent implements OnInit {
 
   name: string = '';
+  todoDisplay: string;
+  todo=[];
   nameDisplay: string = '';
   showSpinner: boolean = false;
+  question: boolean = true;
+  asktodo: boolean = false;
 
   showName() {
     this.showSpinner = true;
+    this.question = false;
 
     setTimeout(()=>{
        this.nameDisplay = this.name;
        this.showSpinner = false;
-    }, 1000)
+       this.asktodo = true;
+    }, 500)
+
+    
+  }
+
+  additem(){
+    this.todo.push(this.todoDisplay);
+    this.todoDisplay='';
+
   }
 
 
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core' ;
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule } from '@angular/material';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
-    imports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule],
-    exports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule],
+    imports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatListModule ],
+    exports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatListModule ],
 })
 
 export class MaterialModule { }
